1b289932e86c390a4cc27ff06d54843410f0a2d1
lindapei2016 <lindapei2016@gmail.com>
2024-12-20 13:49:18 -0800

    Fixed merge conflict in flu_components.

--- flu_demo.py
+++ flu_demo.py
@@ -1,7 +1,7 @@
 import numpy as np
 from pathlib import Path
 
-from flu_components import FluModelConstructor
+from flu_components import FluSubpopModel
 from plotting import create_basic_compartment_history_plot
 
 # Obtain path to folder with JSON input files
@@ -14,18 +14,18 @@ fixed_params_filepath = base_path / "fixed_params.json"
 state_vars_init_vals_filepath = base_path / "state_variables_init_vals.json"
 
 # Create a constructor using these filepaths
-flu_demo_constructor = \
-    FluModelConstructor(config_filepath,
-                        fixed_params_filepath,
-                        state_vars_init_vals_filepath)
+flu_demo_model = \
+    FluSubpopModel(config_filepath,
+                   fixed_params_filepath,
+                   state_vars_init_vals_filepath)
 
-# Create TransmissionModel instance from the constructor,
-#   using a random number generator with starting seed 888888
-#   to generate random variables
-flu_demo_model = flu_demo_constructor.create_transmission_model(888888)
+# Need to add RNG
+flu_demo_model.RNG = np.random.default_rng(88888)
 
 # Simulate 300 days
 flu_demo_model.simulate_until_time_period(300)
+
+breakpoint()
 
 # Plot
 create_basic_compartment_history_plot(flu_demo_model,
--- plotting.py
+++ plotting.py
@@ -6,9 +6,9 @@ def create_basic_compartment_history_plot(sim_model,
                                           savefig_filename=None):
     plt.clf()
     plt.figure(1)
-    for compartment in sim_model.compartments:
+    for name, compartment in sim_model.compartment_lookup.items():
         history_vals_list = [np.sum(age_risk_group_entry) for age_risk_group_entry in compartment.history_vals_list]
-        plt.plot(history_vals_list, label=compartment.name, alpha=0.6)
+        plt.plot(history_vals_list, label=name, alpha=0.6)
     plt.legend()
     plt.xlabel("Days")
     plt.ylabel("Number of individuals")
